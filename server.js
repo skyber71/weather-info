b3ef1d4c110faaf0b1516871e7ea5d4e6119828d
Kiran Yadav <mail.skyber71@gmail.com>
2022-10-05 00:17:16 +0530

    Done

--- server.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const port_num = process.env.PORT;
-
-app.listen(port_num, ()=>{
-    console.log("Backend is running successfully on port " + port_num)
-});
